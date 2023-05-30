2e0ddbc31dc8f20382e6e321ff2b0b48afb6562b
cappochinho <edwardprempeh18@gmail.com>
2023-05-30 21:52:48 +0100

    Task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,31 @@
+const sha1 = require('sha1');
+const mDb = require('mongodb');
+const Redis = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async createUser(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+    if (await dbClient.users.findOne({ email })) return res.status(400).send({ error: 'Already exist' });
+
+    const newUser = await dbClient.users.insertOne({
+      email,
+      password: sha1(password),
+    });
+    return res.status(201).send({ id: newUser.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const authToken = `auth_${req.headers['x-token']}`;
+    const userId = await Redis.get(authToken);
+
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+    const user = await dbClient.users.findOne({ _id: new mDb.ObjectId(userId) });
+    return res.json({ id: user._id, email: user.email });
+  }
+}
+
+module.exports = UsersController;
